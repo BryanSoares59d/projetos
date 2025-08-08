2969e73b2e2abaefe3059384ab7adbd0bd3e8730
BryanSoares59d <bryan.carvalho2@etec.sp.gov.br>
2025-08-08 17:17:15 -0300

    Aula 14 - props

--- primeiro-teste/src/App.js
+++ primeiro-teste/src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/Helloworld';
+import Frase from './Frase';
+import DigaMeuNome from './components/DigaMeunome';
+
+
 
 function App() {
+  const nome = "Rodrigo "
   return (
     <div className="App">
         <h1>Meu primeiro react</h1>
+        <Frase/>
+        <HelloWorld/>
+        <Frase/>
+        <HelloWorld/>
+        <DigaMeuNome nome="Bryan"/>
+        <DigaMeuNome nome={nome}/>
+
     </div>
   );
 }
